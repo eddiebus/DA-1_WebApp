62abf74154a129a42277d8c2054a77d636c35486
eddiebus <edwinbusulwa@gmail.com>
2022-07-07 14:57:11 +0100

    Geolocate Add
    Add Geolocate Element to the map

--- JS/MapJS.js
+++ JS/MapJS.js
@@ -32,6 +32,18 @@ function MapLoad()
     //Turn off X Rotation
     map.setMaxPitch(0);
 
+    let GeoLocateControl = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+    });
+
+    map.addControl(GeoLocateControl);
+
     $.ajax({
         type: "GET",
         url: "PHP//DataGet.php",
