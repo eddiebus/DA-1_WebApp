ee25e048b925468b16a4dc0e60e0b480ab5b2587
eddiebus <edwinbusulwa@gmail.com>
2022-07-11 15:50:18 +0100

    Data View style changes & PHP Plan
    Draft code for PHP. Not Tested!!!
    
    DataView style improved slightly. Close button in better position.

--- PHP/DataPost.php
+++ PHP/DataPost.php
@@ -1,5 +1,22 @@
 <?php
-$databaseDir = "Database/Data.db"
+$databaseDir = "Database/da-1_Data.db";
+
+
+
+//Prepare Log SQL Statement
+$db = new PDO($databaseDir);
+$postString = implode("|", $_POST);
+$query = $db->prepare('
+INSERT INTO RequestLogs (Type,Message)
+VALUES("POST",statement)
+');
+$query->bindParam('statement',$postString);
+$query->execute();
+//Echo Response code
+/*
+ * This is needed to communicate with Digital Matter's servers
+ * If no response is given, devices will think data hasn't been handled
+ */
 echo http_response_code(200);
 
 ?>
