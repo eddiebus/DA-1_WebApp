912f55173a3b1410632052885fc57e0e01cf75bf
eddiebus <edwinbusulwa@gmail.com>
2022-07-15 14:48:16 +0100

    Update DataPost.php

--- PHP/DataPost.php
+++ PHP/DataPost.php
@@ -1,3 +1,20 @@
 <?php
+function CheckPostMessage()
+{
+    if (isset($_POST))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+if (CheckPostMessage() == false)
+{
+    echo "HTTP Request is not type 'POST'";
+}
+
 echo "Hello Post";
 ?>
