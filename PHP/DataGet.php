0c896afda22971ef9db7958193a3db73008d6659
eddiebus <edwinbusulwa@gmail.com>
2022-07-15 13:07:16 +0100

    PHP Update

--- PHP/DataGet.php
+++ PHP/DataGet.php
@@ -1,6 +1,5 @@
 <?php
 
-
 echo json_encode("Hello");
 
 ?>
--- PHP/DataPost.php
+++ PHP/DataPost.php
@@ -1,22 +1,3 @@
 <?php
-$databaseDir = "Database/da-1_Data.db";
-
-
-
-//Prepare Log SQL Statement
-$db = new PDO($databaseDir);
-$postString = implode("|", $_POST);
-$query = $db->prepare('
-INSERT INTO RequestLogs (Type,Message)
-VALUES("POST",statement)
-');
-$query->bindParam('statement',$postString);
-$query->execute();
-//Echo Response code
-/*
- * This is needed to communicate with Digital Matter's servers
- * If no response is given, devices will think data hasn't been handled
- */
 echo http_response_code(200);
-
 ?>
