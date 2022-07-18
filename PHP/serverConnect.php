d3dfa2aae390dea960c8cce04c5c00a0e964001a
eddiebus <edwinbusulwa@gmail.com>
2022-07-18 17:04:18 +0100

    php server connect test
    A test php file. Connect to microsoft azure database. Will echo string of result

---
+++ PHP/serverConnect.php
@@ -0,0 +1,21 @@
+<?php
+//My Server Web Address
+$serverName = "da1devicedb.database.windows.net";
+$connectionOptions = array(
+    //Database name
+    "Database" => "DA1_Data",
+    //User Name
+    "Uid" => "da1Admin",
+    "PWD" => "Da1device!"
+);
+
+$conn = sqlsrv_connect($serverName,$connectionOptions);
+if ($conn == false)
+{
+    echo "Server Connection Success!";
+}
+else
+{
+    echo "Server Connection Error";
+}
+?>
