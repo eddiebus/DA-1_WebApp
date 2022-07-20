db0e58953bc79f50bf3f4bfdadb861b7dba6b2a0
eddiebus <edwinbusulwa@gmail.com>
2022-07-20 15:33:21 +0100

    DataPost Draft

--- PHP/DataPost.php
+++ PHP/DataPost.php
@@ -1,4 +1,6 @@
 <?php
+include 'dbInfo.php';
+
 function CheckPostMessage()
 {
     if ($_SERVER['REQUEST_METHOD'] === 'POST')
@@ -17,5 +19,28 @@ if (!CheckPostMessage())
 }
 else {
     echo "Hello Post";
+
+    $connectionOptions = array(
+        //Database name
+        "Database" => $dbName,
+        //User Name
+        "Uid" => $dbUserName,
+        "PWD" => $dbPassword
+    );
+
+    $conn = sqlsrv_connect($dbAddress,$connectionOptions);
+    if (!$conn)
+    {
+        echo "Server Connection Success!";
+        $query = "INSERT INTO";
+
+        date_default_timezone_set('UTC');
+        $currentTime = date('Y-m-d H:i:s');
+    }
+    else
+    {
+        echo "Server Connection Error";
+    }
+
 }
 ?>
---
+++ PHP/dbInfo.php
@@ -0,0 +1,7 @@
+<?php
+$dbAddress = "da1devicedb.database.windows.net";
+$dbName = "DA1_Data";
+$dbUserName = "da1Admin";
+$dbPassword = "Da1device!";
+
+?>
