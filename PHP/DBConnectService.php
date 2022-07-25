4a7c570a5c0f1b215bf8ab4d8521c5798da6d314
eddiebus <edwinbusulwa@gmail.com>
2022-07-25 13:42:50 +0100

    PHP Database Connection by Class

--- PHP/DBConnectService.php
+++ PHP/DBConnectService.php
@@ -5,7 +5,6 @@ Class that handles connection to azure database.
 Has set functions for manipulating data
 */ 
 
-
 //DB Info (Azure) 
 $dbAddress = "uod-thinkocean.database.windows.net";
 $dbName = "DA1_DB";
@@ -13,16 +12,32 @@ $dbUserName = "UODThinkOceanAdmin";
 $dbPassword = "taAdmin!";
 
 $LogTableName = "idk";
- 
+
+function CheckPostMessage(): bool
+{
+    if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+
+}
+
 class DA1Database{
 
     //Database Connection
     private $dbConn = null;
     public function __construct()
     {
+        global $dbAddress;
+        global $dbName;
+        global $dbUserName;
+        global $dbPassword;
         try {
             $db = new PDO(
-            "sqlsrv:server=$dbAddress;Database=$dbName", 
+            "sqlsrv:server=$dbAddress;Database=$dbName",
             $dbUserName, 
             $dbPassword);
         }
@@ -38,37 +53,31 @@ class DA1Database{
     }
 
     public function LogRequest(){
-        if (!$db)
+        if (!$this->dbConn)
         {
             return;
         }
         else
         {
+            $currentDateTime = date('Y-m-d H:i:s');
+            $message = file_get_contents('php://input');
             $insertQuery = "
             INSERT INTO [dbo].[Logs] (TimeSent,MessageType,Message)
             VALUES ('$currentDateTime','Post','$message');;
             ";
         }
-
-        $result =  $db->query($insertQuery);
+        $result =  $this->dbConn->query($insertQuery);
         if (!$result)
         {
             echo nl2br("PDO Error: Query failed.");
         }
     }
-
-    public function AddDevice()
-    {
-
+    public function AddDevice(){
     }
 
     public function RemoveDevice(){
-        
     }
 
     public function AddDevicePing(){
-
     }
 }
-
-?>
--- PHP/DataPost.php
+++ PHP/DataPost.php
@@ -1,57 +1,6 @@
 <?php
 include 'dbInfo.php';
-require_once('DBCOnnectService.php')
+require_once('DBCOnnectService.php');
 
-function CheckPostMessage(): bool
-{
-    if ($_SERVER['REQUEST_METHOD'] === 'POST')
-    {
-        return TRUE;
-    }
-    else
-    {
-        return FALSE;
-    }
-}
+$conn = new DA1Database();
 
-if (!CheckPostMessage())
-{
-    echo "HTTP Request is not type 'POST'";
-}
-else {
-    $db = null;
-    try {
-        $db = new PDO("sqlsrv:server=$dbAddress;Database=$dbName", $dbUserName, $dbPassword);
-
-        echo "PDO Connection Success"."<br>";
-
-        date_default_timezone_set('UTC');
-        $currentDateTime = date("Y-m-d h:i:s");
-        $message = file_get_contents('php://input');
-
-        $insertQuery = "
-        INSERT INTO [dbo].[Logs] (TimeSent,MessageType,Message)
-        VALUES ('$currentDateTime','Post','$message');;
-        ";
-
-        $result =  $db->query($insertQuery);
-
-        if (!$result)
-        {
-            echo "Statement Error"."<br>";
-            echo "$insertQuery";
-        }
-        else{
-            echo "ok";
-            echo implode($_POST);
-        }
-    }
-    catch (PDOException $error) {
-        echo "PDO Error: ".$error->getMessage()."<br>";
-    }
-    catch (Exception $error){
-        echo "General Connection Error: ".$error->getMessage()."<br>";
-    }
-
-}
-?>
